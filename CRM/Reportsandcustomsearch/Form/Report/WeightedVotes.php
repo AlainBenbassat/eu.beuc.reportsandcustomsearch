0361173b28c3ebe2d0c1da35e09d60571fd153e5
Alain Benbassat <alain@businessandcode.eu>
2025-04-15 11:25:01 +0200

    adds reimbursement

--- CRM/Reportsandcustomsearch/Form/Report/WeightedVotes.php
+++ CRM/Reportsandcustomsearch/Form/Report/WeightedVotes.php
@@ -27,6 +27,7 @@ class CRM_Reportsandcustomsearch_Form_Report_WeightedVotes extends CRM_Report_Fo
       'theoretical_number_of_votes' => 'Theoretical Number of Votes',
       'voting_rights' => 'Voting rights?',
       'number_of_votes' => 'Actual Number of Votes',
+      'reimbursement' => 'Reimbursement?',
     ];
 
     foreach ($colTitles as $k => $colTitle) {
@@ -85,28 +86,16 @@ class CRM_Reportsandcustomsearch_Form_Report_WeightedVotes extends CRM_Report_Fo
   public function alterDisplay(&$rows) {
     $referenceYear = $this->getReferenceYear();
 
-    // build the report from scratch
+    // STEP 1: build the report from scratch
     $rows = [];
 
     $members = new CRM_Reportsandcustomsearch_Members($referenceYear);
     $allMembers = $members->get();
     $totalFees = 0;
 
+    // build the basic table with all the members
     foreach ($allMembers as $member) {
-      $row = [];
-
-      $row['civicrm_dummy_entity_display_name'] = $this->getNameWithLinkToContact($member);
-      $row['civicrm_dummy_entity_country'] = $member['address.country_id:label'];
-      $row['civicrm_dummy_entity_membership_type'] = $member['membership_type_id:label'];
-
-      [$fee, $status] = $members->getMembershipFee($member['contact_id']);
-      $row['civicrm_dummy_entity_membership_fee'] = $fee;
-      $row['civicrm_dummy_entity_membership_fee_percentage'] = '';
-      $row['civicrm_dummy_entity_membership_fee_status'] = $status;
-      $row['civicrm_dummy_entity_theoretical_number_of_votes'] =  0;
-      $row['civicrm_dummy_entity_voting_rights'] = '';
-      $row['civicrm_dummy_entity_number_of_votes'] =  0;
-
+      $row = $this->buildNewRow($members, $member);
       $totalFees += $row['civicrm_dummy_entity_membership_fee'];
 
       $rows[] = $row;
@@ -115,93 +104,35 @@ class CRM_Reportsandcustomsearch_Form_Report_WeightedVotes extends CRM_Report_Fo
     $totalNumberOfVotes = 0;
     $totalVotingRights = 0;
 
+    // STEP 2: loop over the table and calculate voting rights
     for ($i = 0; $i < count($rows); $i++) {
-      if ($totalFees > 0) {
-        $percentageFee = round($rows[$i]['civicrm_dummy_entity_membership_fee'] / $totalFees * 100, 2);
-      }
-      else {
-        $percentageFee = 0;
-      }
-
-      if ($percentageFee <= 1) {
-        $theoreticalNumVotes = 1;
-      }
-      elseif ($percentageFee <= 5) {
-        $theoreticalNumVotes = 3;
-      }
-      else {
-        $theoreticalNumVotes = 5;
-      }
+      $percentageFee = $this->calculatePercentageFee($totalFees, $rows[$i]);
+      $theoreticalNumVotes = $this->calculateTheoreticalNumberOfVotes($percentageFee);
 
       $rows[$i]['civicrm_dummy_entity_membership_fee_percentage'] =  $percentageFee;
+      $rows[$i]['civicrm_dummy_entity_voting_rights'] = $this->determineVotingRights($rows[$i]);
+      $rows[$i]['civicrm_dummy_entity_theoretical_number_of_votes'] = $this->determineTheoreticalNumberOfVotes($rows[$i], $theoreticalNumVotes);
+      $rows[$i]['civicrm_dummy_entity_number_of_votes'] = $this->determineNumberOfVotes($rows[$i], $theoreticalNumVotes);
+      $rows[$i]['civicrm_dummy_entity_reimbursement'] = $this->determineReimbursement($rows[$i]);
 
-      if ($rows[$i]['civicrm_dummy_entity_membership_type'] !== 'Full Member') {
-        $rows[$i]['civicrm_dummy_entity_voting_rights'] =  'No, not a full member';
-        $rows[$i]['civicrm_dummy_entity_theoretical_number_of_votes'] =  0;
-        $rows[$i]['civicrm_dummy_entity_number_of_votes'] =  0;
-      }
-      elseif ($rows[$i]['civicrm_dummy_entity_membership_fee'] == 0) {
-        $rows[$i]['civicrm_dummy_entity_voting_rights'] =  'No, no fee';
-        $rows[$i]['civicrm_dummy_entity_theoretical_number_of_votes'] =  0;
-        $rows[$i]['civicrm_dummy_entity_number_of_votes'] =  0;
-      }
-      elseif ($rows[$i]['civicrm_dummy_entity_membership_fee_status'] !== 'Paid') {
-        $rows[$i]['civicrm_dummy_entity_voting_rights'] =  'No, not paid';
-        $rows[$i]['civicrm_dummy_entity_theoretical_number_of_votes'] =  $theoreticalNumVotes;
-        $rows[$i]['civicrm_dummy_entity_number_of_votes'] =  0;
-      }
-      else {
-        $rows[$i]['civicrm_dummy_entity_voting_rights'] =  'Yes';
+      // adjust totals
+      if ($rows[$i]['civicrm_dummy_entity_voting_rights'] == 'Yes') {
         $totalVotingRights++;
-
-        $rows[$i]['civicrm_dummy_entity_theoretical_number_of_votes'] =  $theoreticalNumVotes;
-        $rows[$i]['civicrm_dummy_entity_number_of_votes'] =  $theoreticalNumVotes;
       }
-
       $totalTheoreticalNumberOfVotes += $rows[$i]['civicrm_dummy_entity_theoretical_number_of_votes'];
       $totalNumberOfVotes += $rows[$i]['civicrm_dummy_entity_number_of_votes'];
     }
 
-    // totals
-    $row = [
-      'civicrm_dummy_entity_display_name' => 'TOTAL',
-      'civicrm_dummy_entity_country' => '',
-      'civicrm_dummy_entity_membership_type' => '',
-      'civicrm_dummy_entity_membership_fee' => $totalFees,
-      'civicrm_dummy_entity_membership_fee_percentage' => '',
-      'civicrm_dummy_entity_membership_fee_status' => '',
-      'civicrm_dummy_entity_theoretical_number_of_votes' => $totalTheoreticalNumberOfVotes,
-      'civicrm_dummy_entity_voting_rights' => $totalVotingRights,
-      'civicrm_dummy_entity_number_of_votes' => $totalNumberOfVotes,
-    ];
+    // STEP 3: add rows with totals and quorum
+    $row = $this->buildNewTotalsRow($totalFees, $totalTheoreticalNumberOfVotes, $totalVotingRights, $totalNumberOfVotes);
     $this->makeBold($row);
     $rows[] = $row;
 
-    $row = [
-      'civicrm_dummy_entity_display_name' => '',
-      'civicrm_dummy_entity_country' => '',
-      'civicrm_dummy_entity_membership_type' => '',
-      'civicrm_dummy_entity_membership_fee' => '',
-      'civicrm_dummy_entity_membership_fee_percentage' => '',
-      'civicrm_dummy_entity_membership_fee_status' => 'MINIMUM FOR 1/2',
-      'civicrm_dummy_entity_theoretical_number_of_votes' => ceil($totalTheoreticalNumberOfVotes * 1 / 2),
-      'civicrm_dummy_entity_voting_rights' => ceil($totalVotingRights * 1 / 2),
-      'civicrm_dummy_entity_number_of_votes' => ceil($totalNumberOfVotes * 1 / 2),
-    ];
+    $row = $this->buildNewQuorumRow(1, 2, $totalTheoreticalNumberOfVotes, $totalVotingRights, $totalNumberOfVotes);
     $this->makeBold($row);
     $rows[] = $row;
 
-    $row = [
-      'civicrm_dummy_entity_display_name' => '',
-      'civicrm_dummy_entity_country' => '',
-      'civicrm_dummy_entity_membership_type' => '',
-      'civicrm_dummy_entity_membership_fee' => '',
-      'civicrm_dummy_entity_membership_fee_percentage' => '',
-      'civicrm_dummy_entity_membership_fee_status' => 'MINIMUM FOR 2/3',
-      'civicrm_dummy_entity_theoretical_number_of_votes' => ceil($totalTheoreticalNumberOfVotes * 2 / 3),
-      'civicrm_dummy_entity_voting_rights' => ceil($totalVotingRights * 2 / 3),
-      'civicrm_dummy_entity_number_of_votes' => ceil($totalNumberOfVotes * 2 / 3),
-    ];
+    $row = $this->buildNewQuorumRow(2, 3, $totalTheoreticalNumberOfVotes, $totalVotingRights, $totalNumberOfVotes);
     $this->makeBold($row);
     $rows[] = $row;
 
@@ -216,6 +147,135 @@ class CRM_Reportsandcustomsearch_Form_Report_WeightedVotes extends CRM_Report_Fo
 
   public function statistics(&$rows) {
     return [];
+  }
+
+  private function buildNewRow(CRM_Reportsandcustomsearch_Members $members, $member) {
+    $row = [];
+
+    $row['civicrm_dummy_entity_display_name'] = $this->getNameWithLinkToContact($member);
+    $row['civicrm_dummy_entity_country'] = $member['address.country_id:label'];
+    $row['civicrm_dummy_entity_membership_type'] = $member['membership_type_id:label'];
+
+    [$fee, $status] = $members->getMembershipFee($member['contact_id']);
+    $row['civicrm_dummy_entity_membership_fee'] = $fee;
+    $row['civicrm_dummy_entity_membership_fee_percentage'] = '';
+    $row['civicrm_dummy_entity_membership_fee_status'] = $status;
+    $row['civicrm_dummy_entity_theoretical_number_of_votes'] =  0;
+    $row['civicrm_dummy_entity_voting_rights'] = '';
+    $row['civicrm_dummy_entity_number_of_votes'] =  0;
+    $row['civicrm_dummy_entity_reimbursement'] =  '';
+
+    return $row;
+  }
+
+  private function calculatePercentageFee($totalFees, $row) {
+    if ($totalFees > 0) {
+      $percentageFee = round($row['civicrm_dummy_entity_membership_fee'] / $totalFees * 100, 2);
+    }
+    else {
+      $percentageFee = 0;
+    }
+
+    return $percentageFee;
+  }
+
+  private function calculateTheoreticalNumberOfVotes($percentageFee) {
+    if ($percentageFee <= 1) {
+      $theoreticalNumVotes = 1;
+    }
+    elseif ($percentageFee <= 5) {
+      $theoreticalNumVotes = 3;
+    }
+    else {
+      $theoreticalNumVotes = 5;
+    }
+
+    return $theoreticalNumVotes;
+  }
+
+  private function determineVotingRights($row) {
+    if ($row['civicrm_dummy_entity_membership_type'] !== 'Full Member') {
+      return 'No, not a full member';
+    }
+
+    if ($row['civicrm_dummy_entity_membership_fee'] == 0) {
+      return 'No, no fee';
+    }
+
+    if ($row['civicrm_dummy_entity_membership_fee_status'] !== 'Paid') {
+      return 'No, not paid';
+    }
+
+    return 'Yes';
+  }
+
+  private function determineTheoreticalNumberOfVotes($row, $theoreticalNumVotes) {
+    if ($row['civicrm_dummy_entity_membership_type'] == 'Full Member') {
+      return $theoreticalNumVotes;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  private function determineNumberOfVotes($row, $theoreticalNumVotes) {
+    if ($row['civicrm_dummy_entity_membership_type'] == 'Full Member' && $row['civicrm_dummy_entity_membership_fee_status'] == 'Paid') {
+      return $theoreticalNumVotes;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  private function determineReimbursement($row) {
+    if ($row['civicrm_dummy_entity_membership_fee_status'] != 'Paid') {
+      return '';
+    }
+
+    $reimbursementFor = [];
+    $priceFields = \Civi\Api4\PriceField::get(FALSE)
+      ->addSelect('label', 'price_field_value.amount')
+      ->addJoin('PriceFieldValue AS price_field_value', 'INNER')
+      ->addWhere('price_set_id:name', '=', 'Reimbursement_Events')
+      ->addOrderBy('price_field_value.amount', 'ASC')
+      ->execute();
+    foreach ($priceFields as $priceField) {
+      if ($row['civicrm_dummy_entity_membership_fee'] < $priceField['price_field_value.amount']) {
+        $reimbursementFor[] = $priceField['label'];
+      }
+    }
+
+    return implode(', ', $reimbursementFor);
+  }
+
+  private function buildNewTotalsRow($totalFees, $totalTheoreticalNumberOfVotes, $totalVotingRights, $totalNumberOfVotes) {
+    return [
+      'civicrm_dummy_entity_display_name' => 'TOTAL',
+      'civicrm_dummy_entity_country' => '',
+      'civicrm_dummy_entity_membership_type' => '',
+      'civicrm_dummy_entity_membership_fee' => $totalFees,
+      'civicrm_dummy_entity_membership_fee_percentage' => '',
+      'civicrm_dummy_entity_membership_fee_status' => '',
+      'civicrm_dummy_entity_theoretical_number_of_votes' => $totalTheoreticalNumberOfVotes,
+      'civicrm_dummy_entity_voting_rights' => $totalVotingRights,
+      'civicrm_dummy_entity_number_of_votes' => $totalNumberOfVotes,
+      'civicrm_dummy_entity_reimbursement' => '',
+    ];
+  }
+
+  private function buildNewQuorumRow($qTop, $qBottom, $totalTheoreticalNumberOfVotes, $totalVotingRights, $totalNumberOfVotes) {
+    return [
+      'civicrm_dummy_entity_display_name' => '',
+      'civicrm_dummy_entity_country' => '',
+      'civicrm_dummy_entity_membership_type' => '',
+      'civicrm_dummy_entity_membership_fee' => '',
+      'civicrm_dummy_entity_membership_fee_percentage' => '',
+      'civicrm_dummy_entity_membership_fee_status' => "MINIMUM FOR $qTop/$qBottom",
+      'civicrm_dummy_entity_theoretical_number_of_votes' => ceil($totalTheoreticalNumberOfVotes * $qTop / $qBottom),
+      'civicrm_dummy_entity_voting_rights' => ceil($totalVotingRights * $qTop / $qBottom),
+      'civicrm_dummy_entity_number_of_votes' => ceil($totalNumberOfVotes * $qTop / $qBottom),
+      'civicrm_dummy_entity_reimbursement' => '',
+    ];
   }
 
   private function makeBold(&$row) {
